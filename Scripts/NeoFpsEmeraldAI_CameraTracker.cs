0b6ba756f105367386f650897a0c191eeded4ba4
Yondernauts Games <47190251+YondernautsGames@users.noreply.github.com>
2023-03-06 17:32:01 +0000

    Added a UI camera tracker
    The camera tracker will automtically tie into neo's camera changed events and send the correct camera to emerald's health bars so they can properly align

---
+++ Scripts/NeoFpsEmeraldAI_CameraTracker.cs
@@ -0,0 +1,37 @@
+﻿using EmeraldAI;
+using EmeraldAI.Utility;
+using UnityEngine;
+
+namespace NeoFPS.EmeraldAI
+{
+    public static class NeoFpsEmeraldAI_CameraTracker
+    {
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.AfterSceneLoad)]
+        private static void AttachCameraTracker()
+        {
+            FirstPersonCameraBase.onCurrentCameraChanged += OnFirstPersonCameraChanged;
+        }
+
+        private static bool m_Set = false;
+
+        private static void OnFirstPersonCameraChanged(FirstPersonCameraBase cam)
+        {
+            if (cam != null)
+            {
+                if (cam is FirstPersonCamera)
+                {
+                    EmeraldAIHealthBar.m_Camera = cam.unityCamera;
+                    m_Set = true;
+                }
+            }
+            else
+            {
+                if (m_Set)
+                {
+                    EmeraldAIHealthBar.m_Camera = Camera.main;
+                    m_Set = false;
+                }
+            }
+        }
+    }
+}
