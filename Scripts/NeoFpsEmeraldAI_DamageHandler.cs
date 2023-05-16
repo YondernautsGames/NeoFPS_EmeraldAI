251feb850da426c46be93ceca0b2250318a7d58c
Yondernauts Games <47190251+YondernautsGames@users.noreply.github.com>
2023-05-16 10:52:33 +0100

    Fixed semi-colon compilation error

--- Scripts/NeoFpsEmeraldAI_DamageHandler.cs
+++ Scripts/NeoFpsEmeraldAI_DamageHandler.cs
@@ -114,7 +114,7 @@ namespace NeoFPS.EmeraldAI
 						m_EmeraldAISystem.Damage(
 							scaledDamage,
 							EmeraldAISystem.TargetType.Player,
-							(source.controller as Component).transform;
+							(source.controller as Component).transform
 							);
 					}
 					catch (Exception e)
