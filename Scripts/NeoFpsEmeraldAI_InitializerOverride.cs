b95c9f637cf6f14c800c3522ea35fcf16c9bf9bb
Yondernauts Games <47190251+YondernautsGames@users.noreply.github.com>
2024-04-14 11:42:32 +0100

    Update NeoFpsEmeraldAI_InitializerOverride.cs
    Made it more obvious this class is legacy and re-added comments to methods (those should not have been removed)

--- Scripts/NeoFpsEmeraldAI_InitializerOverride.cs
+++ Scripts/NeoFpsEmeraldAI_InitializerOverride.cs
@@ -18,12 +18,33 @@ using EmeraldAI;
 using EmeraldAI.Utility;
 using UnityEngine;
 
+// This class is a legacy workaround to enable location based damage (eg headshots) to Emerald AI characters.
+
+// NOTE: THIS CLASS IS NO LONGER REQUIRED WITH THE LATEST VERSIONS OF EMERALD AI 3, SINCE IT ADDS AN OFFICIAL WAY TO HANDLE LIMB COLLIDERS AND RAGDOLL
+
+// Using this class requires modifying the Emerald AI source code, so it is sensitive to changes in the API.
+// To use it you will need to perform the following steps:
+// 1 - In the EmeraldAIInitializer.cs script, make the EmeraldComponent protected
+// 2 - Make the EnableRagdoll() and DisableRagdoll() methods virtual
+// 3 - Delete the /* and */ comment start and end below to uncomment the methods in this class
+// 4 - Replace the Emerald AI Initializer component on your AI with this class
+// 5 - Add any ragdoll colliders to the m_RagdollColliders array in the inspector
+// 6 - Move the root object of the Emerald AI character (only the root object) to the CharacterControllers layer
+// 7 - Remove the NeoFpsEmeraldAI_DamageHandler component from the root object of the character
+// 8 - Add damage handlers to the character's skeleton (eg, limbs, head and torso)
+//   a - Create a new child object of the relevant bone and set it to the CharacterPhysics layer
+//   b - Add a collider that is roughly the shape required
+//   c - Add a NeoFpsEmeraldAI_DamageHandler component and set it up with the desired values (note that the Emerald demo characters have 10 health, so a multiplier of less than 1 is used in most cases)
+
 namespace NeoFPS.EmeraldAI
 {
+
     public class NeoFpsEmeraldAI_InitializerOverride : EmeraldAIInitializer
     {
         [SerializeField]
         private Collider[] m_RagdollColliders = { };
+
+        /*
 
         public override void DisableRagdoll()
         {
@@ -65,20 +86,7 @@ namespace NeoFPS.EmeraldAI
                 EmeraldComponent.EmeraldEventsManagerComponent.CreateDroppableWeapon();
             }
         }
-
-        // This class is a workaround to enable location based damage (eg headshots) to Emerald AI characters.
-        // Using this class requires modifying the Emerald AI source code, so it is sensitive to changes in the API.
-        // To use it you will need to perform the following steps:
-        // 1 - In the EmeraldAIInitializer.cs script, make the EmeraldComponent protected
-        // 2 - Make the EnableRagdoll() and DisableRagdoll() methods virtual
-		// 3 - Uncomment the code above
-        // 4 - Replace the Emerald AI Initializer component on your AI with this class
-        // 5 - Add any ragdoll colliders to the m_RagdollColliders array in the inspector
-        // 6 - Move the root object of the Emerald AI character (only the root object) to the CharacterControllers layer
-        // 7 - Remove the NeoFpsEmeraldAI_DamageHandler component from the root object of the character
-        // 8 - Add damage handlers to the character's skeleton (eg, limbs, head and torso)
-        //   a - Create a new child object of the relevant bone and set it to the CharacterPhysics layer
-        //   b - Add a collider that is roughly the shape required
-        //   c - Add a NeoFpsEmeraldAI_DamageHandler component and set it up with the desired values (note that the Emerald demo characters have 10 health, so a multiplier of less than 1 is used in most cases)
+	
+        */
     }
 }
